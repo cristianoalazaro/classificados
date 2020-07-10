ea098564c0cee6ee315ef3847e3d161b52c2a72b
Cristiano Ap Lazaro <cristianoalazaro@hotmail.com>
2020-07-10 09:21:51 -0300

    Commit1007

--- views/template.php
+++ views/template.php
@@ -28,7 +28,7 @@
         <script type="text/javascript" src="<?php echo 
         BASE_URL; ?>/assets/js/jquery.min.js"></script>
         <script type="text/javascript" src="<?php echo 
-        BASE_URL; ?>/assets/js/bootstrap.js"></script>
+        BASE_URL; ?>/assets/js/bootstrap.min.js"></script>
         <script type="text/javascript" src="<?php echo 
         BASE_URL; ?>/assets/js/script.js"></script>
 
